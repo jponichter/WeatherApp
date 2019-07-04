3a988f2966efdeed8bf69f6efee8bb8d0ab9baf0
Jan Ponichterj <j.ponichter@wp.pj>
2019-07-04 12:23:40 +0200

    Basic HTML/CSS added and API connected.

---
+++ WeatherApp/WeatherApp/js/main.js
@@ -0,0 +1,34 @@
+﻿window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector('.location-timezone');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/743a1f76b7b781ec1fe4859412543a96/${lat},${long}`;
+            console.log(api);
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    const { temperature, summary } = data.currently;
+                    //Set DOM Elements from API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                });
+        })
+    } else {
+        h1.textContent = "This is not working";
+    };
+
+    
+});
