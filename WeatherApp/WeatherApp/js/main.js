65c151604edca6b86ae5a90f724a684435b17146
Jan Ponichter <ponichterjot@gmail.com>
2019-07-19 01:55:20 +0200

    Everything reworked. Changing degree fixed.

--- WeatherApp/WeatherApp/js/main.js
+++ WeatherApp/WeatherApp/js/main.js
@@ -4,6 +4,9 @@ let temperatureSection = document.querySelector('.temperature');
 const temperatureSpan = document.querySelector('.temperature span')
 
 let details;
+let temperature;
+let minTemperature;
+let maxTemperature;
 
 
 window.addEventListener('load', () => {
@@ -27,27 +30,21 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     console.log(data);
-                    const { temperature, summary, icon } = data.currently;
+
+                    details = data;
+                    const { summary, icon } = details.currently;
                     //Set DOM Elements from API
-                    temperatureDegree.textContent = temperature;
+                    maxTemperature = details.daily.data[0].temperatureMax;
+                    minTemperature = details.daily.data[0].temperatureMin;
+
+                    temperatureDegree.textContent = Math.floor((minTemperature - 32) * (5 / 9)) + ' - ' + Math.floor((maxTemperature - 32) * (5 / 9));
                     temperatureDescription.textContent = summary;
-                    locationTimezone.textContent = data.timezone;
-                    //Formula for Celsius
-                    let celsius = (temperature - 32) * (5 / 9);
+                    locationTimezone.textContent = details.timezone;
+
+                    
+
                     //Set Icon
                     setIcons(icon, document.querySelector(".icon"));
-
-                    //Change temperature to Celsius
-                    temperatureSection.addEventListener('click', () => {
-                        if (temperatureSpan.textContent === "F") {
-                            temperatureSpan.textContent = "C";
-                            temperatureDegree.textContent = Math.floor(celsius);
-                        } else {
-                            temperatureSpan.textContent = "F";
-                            temperatureDegree.textContent = temperature;
-                        }
-                    })
-                    details = data;
                 });
         })
     } else {
@@ -55,6 +52,7 @@ window.addEventListener('load', () => {
     }
 
     function setIcons(icon, iconID) {
+
         const skycons = new Skycons({ color: "white" });
         const currentIcon = icon.replace(/-/g, "_").toUpperCase();
         skycons.play();
@@ -62,16 +60,38 @@ window.addEventListener('load', () => {
     }
 });
 
+
+
+//Change temperature to Celsius
+function degreeConverter() {
+    if (temperatureSpan.textContent === "C") {
+        temperatureSpan.textContent = "F";
+        temperatureDegree.textContent = minTemperature + ' - ' + maxTemperature;
+    } else if (temperatureSpan.textContent === "F"){
+        temperatureSpan.textContent = "C";
+        temperatureDegree.textContent = Math.floor((minTemperature - 32) * (5 / 9)) + ' - ' + Math.floor((maxTemperature - 32) * (5 / 9));
+    }
+}
+temperatureSection.addEventListener('click', degreeConverter);
+
+
 function selectDay() {
     let day = document.getElementById('day-dropdown').value;    
-    let daily = details.daily.data;
-    let highTemperature = daily[day].temperatureHigh;
-    let lowTemperature = daily[day].temperatureLow;
-    let daySummary = daily[day].summary;
-    let dayIcon = daily[day].icon.replace(/-/g, "_").toUpperCase();
-    console.log(lowTemperature, daySummary);
+    let daily = details.daily.data[day];
+    maxTemperature = daily.temperatureMax;
+    minTemperature = daily.temperatureMin;
+    let daySummary = daily.summary;
+    let dayIcon = daily.icon.replace(/-/g, "_").toUpperCase();
+    console.log(minTemperature, daySummary);
 
-    temperatureDegree.textContent = lowTemperature + ' - ' + highTemperature;
+    if (temperatureSpan.textContent === "F") {
+        temperatureSpan.textContent = "F";
+        temperatureDegree.textContent = minTemperature + ' - ' + maxTemperature;
+    } else if (temperatureSpan.textContent === "C") {
+        temperatureSpan.textContent = "C";
+        temperatureDegree.textContent = Math.floor((minTemperature - 32) * (5 / 9)) + ' - ' + Math.floor((maxTemperature - 32) * (5 / 9));
+    }
+    //temperatureDegree.textContent = Math.floor((minTemperature - 32) * (5 / 9)) + ' - ' + Math.floor((maxTemperature - 32) * (5 / 9));
     temperatureDescription.textContent = daySummary;
 };
 
